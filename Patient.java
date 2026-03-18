8916ea811fb0f98455140d699dd7e710e240d362
seynl77 <lixingyuan1998@gmail.com>
2026-03-17 20:52:52 -0700

    added basic methods for patient class

--- Patient.java
+++ Patient.java
@@ -1,3 +1,26 @@
-public class Patient {
+public class Patient extends Person {
+    private String medicalHistory;
+    private boolean admitted;
 
+    public void admitPatient() {
+        admitted = true;
+        System.out.println(getName() + " has been admitted.");
+    }
+
+    public void dischargePatient() {
+        admitted = false;
+        System.out.println(getName() + " has been discharged.");
+    }
+
+    public void updateMedicalHistory(String history) {
+        if (history != null && !history.isEmpty()) {
+            medicalHistory = history;
+        }
+    }
+
+    public void viewPatientStatus() {
+        System.out.println("Patient: " + getName());
+        System.out.println("Admitted: " + admitted);
+        System.out.println("Medical History: " + medicalHistory);
+    }
 }
