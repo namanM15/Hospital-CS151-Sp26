5006e82a3b68d44538c8ad4b44c20227f43d5321
Derek <derekyeh2004@gmail.com>
2026-03-28 11:19:56 -0700

    add limit of 100 instances in Hospital Class

--- src/model/Hospital.java
+++ src/model/Hospital.java
@@ -1,7 +1,11 @@
 package src.model;
 import java.util.ArrayList;
 
+import src.exceptions.MaxCapacityException;
+
 public class Hospital {
+    public static final int MAX_INSTANCES = 100;
+
     private String name;
     private ArrayList<Patient> patients;
     private ArrayList<Doctor> doctors;
@@ -18,24 +22,44 @@ public class Hospital {
         rooms = new ArrayList<>();
     }
 
-    public void addPatient(Patient patient) {
+    public void addPatient(Patient patient) throws MaxCapacityException {
+        if (patients.size() >= MAX_INSTANCES) {
+            throw new MaxCapacityException("patient");
+        }
         patients.add(patient);
+        System.out.println("Patient " + patient.getName() + " added.");
     }
 
-    public void addDoctor(Doctor doctor) {
+    public void addDoctor(Doctor doctor) throws MaxCapacityException {
+        if (doctors.size() >= MAX_INSTANCES) {
+            throw new MaxCapacityException("doctor");
+        }
         doctors.add(doctor);
+        System.out.println("Doctor " + doctor.getName() + " added.");
     }
 
-    public void addAppointment(Appointment appointment) {
+    public void addAppointment(Appointment appointment) throws MaxCapacityException {
+        if (appointments.size() >= MAX_INSTANCES) {
+            throw new MaxCapacityException("appointment");
+        }
         appointments.add(appointment);
+        System.out.println("Appointment added.");
     }
 
-    public void addBill(Bill bill) {
+    public void addBill(Bill bill) throws MaxCapacityException {
+        if (bills.size() >= MAX_INSTANCES) {
+            throw new MaxCapacityException("bill");
+        }
         bills.add(bill);
+        System.out.println("Bill added.");
     }
 
-    public void addRoom(Room room) {
+    public void addRoom(Room room) throws MaxCapacityException {
+        if (rooms.size() >= MAX_INSTANCES) {
+            throw new MaxCapacityException("room");
+        }
         rooms.add(room);
+        System.out.println("Room added.");
     }
 
     public String getName() {
